3f641e5d1669bf09341981680bb55f49f8bfad14
Wayne Tasaki <waynetasaki@gmail.com>
2022-07-20 10:20:32 -0700

    desktop almost complete

--- test.js
+++ test.js
@@ -1,61 +1,31 @@
-gsap.registerPlugin(ScrollTrigger);
-		const locoScroll = new LocomotiveScroll({
-			el: document.querySelector("[data-scroll-container]"),
-			smooth: true,
-			getSpeed: true,
-			getDirection: true,
-            inertia: 0.75,
-			useKeyboard: true
-		});
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+let controller = new ScrollMagic.Controller();
 
-		locoScroll.update();
 
-//console.log(ScrollTrigger.scrollerProxy)
 
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
+///////Extensions:
+TimelineLite.prototype.wait = function (position) {
+  var time = this.duration();
+  return this.set({}, {}, time + position);
+};
+////////////////////////
 
-// tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy("[data-scroll-container]", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector("[data-scroll-container]").style.transform ? "transform" : "fixed"
+var popup = new TimelineLite();
+popup
+  .wait(1)
+  .from(".bubble", 0.8, { scale: 0, ease: Elastic.easeOut, x: "550px" });
+
+  let jiggle = new TimelineMax({ repeat: -1, repeatDelay: 3 });
+jiggle.to(".bubble", 0.1, {
+  y: "-=5",
+  yoyo: true,
+  repeat: 5,
+  ease: Sine.easeInOut,
 });
 
-
-// Layered Pinning from Bottom
-gsap.to(".panel", {
-  yPercent: -100, 
-  ease: "none",
-  stagger: 0.5,
-  scrollTrigger: {
-    trigger: "#container",
-    start: "top top",
-    end: "+=300%",
-    scrub: true,
-    pin: true,
-    scroller: '[data-scroll-container]'  // * //     
-  }
-});
-
-ScrollTrigger.create({
-    trigger: "section.content",
-    start: "top top",
-    end: "+=100%",
-    pin: true,
-    scroller: '[data-scroll-container]'  // * //   
+new ScrollMagic.Scene({
+  triggerElement: ".contact",
+  repeat: Infinity,
 })
-
-gsap.set(".panel", {zIndex: (i, target, targets) => targets.length - i});
-
-
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
+  .setTween(popup)
+  .addTo(controller);
