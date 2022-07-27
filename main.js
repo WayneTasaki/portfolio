2dbdff2d9330d2b2e2e840023733211a4b184841
Wayne Tasaki <waynetasaki@gmail.com>
2022-07-27 13:45:52 -0700

    scroll to top button added

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ let controller = new ScrollMagic.Controller();
 
 // CHANGE ALL VAR TO LET OR CONST
 // COMMENT WHAT EVERYTHING DOES
+// have contact icons change on mouseover
 
 ///////Extensions:
 TimelineLite.prototype.wait = function (position) {
@@ -82,3 +83,27 @@ window.onload = function () {
         });
     });
 };
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    // mybutton.style.display = "block";
+    mybutton.style.opacity = "1";
+    mybutton.style.visibility = "visible";
+  } else {
+    // mybutton.style.display = "none";
+    mybutton.style.opacity = "0";
+    mybutton.style.visibility = "hidden";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
